3286825b54e5a8afff40e3d7d470f917f894eb21
Matthew Wayock <wayock@gmail.com>
2019-11-16 22:21:48 -0500

    connected database for registering users

--- detecting-faces-api/server.js
+++ detecting-faces-api/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
 const cors = require ('cors');
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'wayock',
+        password : '',
+        database : 'smart-brain'
+    }
+});
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 const app = express();
 
@@ -55,15 +70,18 @@ app.post('/register', (req, res) => {
             console.log(hash);
         });
     });
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
     })
-    res.json(database.users[database.users.length-1]);
+    .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('Unable to register.'))
+    
 })
 
 app.get('/profile/:id', (req, res) => {
